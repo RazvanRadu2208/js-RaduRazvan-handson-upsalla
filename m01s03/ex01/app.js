52a2912fa9fbe394026a9d59c6951992adaa2da6
RazvanRadu2208 <razvanradu2208@gmail.com>
2023-09-07 23:09:00 +0300

    07.09

---
+++ m01s03/ex01/app.js
@@ -0,0 +1,18 @@
+var object = {
+  name: 'Ion',
+  surname: 'Ionut',
+  age: 20,
+};
+
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' ' +
+    'si am' +
+    ' ' +
+    object.age +
+    ' ' +
+    'ani',
+);
